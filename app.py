ea730455052aa83362c87e1580eaa939888d67f2
hoang vu <stm12p2@gmail.com>
2018-04-23 14:25:04 +1000

    create post api

--- app.py
+++ app.py
@@ -9,5 +9,17 @@ app = Flask(__name__)
 def hello_world(username=None):
     return("Hello")
 
+@app.route('/predict', methods = ['POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            lin_reg = joblib.load("./linear_simple.pkl")
+        except ValueError:
+            print("value error encountered")
+
+        return print(data)
+        #return jsonify(lin_reg.predict(x_pred).tolist())
+
 if __name__ == "__main__":
     app.run()
