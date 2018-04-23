a7364bdd93d9678badcb559cad9b18f6e4e41b98
hoang vu <stm12p2@gmail.com>
2018-04-23 15:26:20 +1000

    test api

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
 """Filename: app.py
   """
 
-from flask import Flask
-import requests
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world(username=None):
     return("Hello")
 
-@app.route('/predict', methods = ['POST'])
-def predict():
+@app.route('/get', methods = ['GET'])
+def get():
+    if request.method == 'GET':
+        try:
+            r = request.args.get('temp')
+        except ValueError:
+            print("erros")
+    return(r * 10)
+
+@app.route('/post', methods = ['POST'])
+def post():
     if request.method == 'POST':
         try:
-            data = requests.get_json()
-            #lin_reg = joblib.load("./linear_simple.pkl")
+            data = "lkjasd"
         except ValueError:
             print("value error encountered")
 
-        return print(data)
+        return(r)
         #return jsonify(lin_reg.predict(x_pred).tolist())
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="127.0.0.1", debug=True, port=1200)
