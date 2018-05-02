1564330c69b53dba54b89ab7805a3ce60a379ef2
hoang vu <stm12p2@gmail.com>
2018-05-02 10:48:52 +1000

    remove cross origin

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def hello_world(username=None):
     return("Hello")
 
 @app.route('/get', methods = ['GET'])
-@cross_origin(origin='https://aspronto-pal.ml', headers=['Content-Type','Authorization'])
+#@cross_origin(origin='https://aspronto-pal.ml', headers=['Content-Type','Authorization'])
 def get():
     if request.method == 'GET':
         try:
